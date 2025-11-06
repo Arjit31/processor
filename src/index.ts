0cdacbdeb82a4d6bbcedc23aefc757620af375e8
Arjit31 <apk20023110@gmail.com>
2025-11-06 22:10:36 +0530

    add producer code

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import {Kafka} from "kafkajs"
+
+const prisma = new PrismaClient();
+
+const TOPIC_NAME = "zap-events"
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092']
+})
+const producer = kafka.producer()
+
+
+async function main() {
+    await producer.connect()
+    while (true) {
+        const pendingRows = await prisma.zapRunOutbox.findMany({
+            take: 10,
+        });
+        await producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => {
+                return {
+                    value: r.zapRunId
+                }
+            }),
+        })
+    }
+}
+
+main();
